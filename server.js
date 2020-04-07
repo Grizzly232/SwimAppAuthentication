a17ac001b5d1a082fe8585a1fc5c3818e599adca
Michael Brady <michael.brady.ie@gmail.com>
2020-04-07 14:37:30 +0100

    js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const httpServer = require('http')
+
+const requestListener = function(req, res) {
+    res.writeHead(200);
+    res.end("Hello World")
+}
+
+const port = process.env.PORT || 3000;
+const server = httpServer.createServer();
+server.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
