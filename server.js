812250ee795e1e66d07c482e245db03d26b01ced
Michael Brady <michael.brady.ie@gmail.com>
2020-04-07 15:09:48 +0100

    backend functioning

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
-const httpServer = require('http')
+require('rootpath')();
+const express = require('express');
+const app = express();
+const httpServer = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('./src/_helpers/jwt');
+const errorHandler = require('./src/_helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(jwt());
+
+app.use('/users', require('./users/users.controller'));
+
+app.use(errorHandler);
+
 
 const requestListener = function(req, res) {
     res.writeHead(200);
@@ -6,7 +24,6 @@ const requestListener = function(req, res) {
 }
 
 const port = process.env.PORT || 3000;
-const server = httpServer.createServer(requestListener);
-server.listen(port, function () {
+const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
---
+++ users/user.service.js
@@ -0,0 +1,55 @@
+const config = require('./../config.json');
+const jwt = require('jsonwebtoken');
+const firebase = require('firebase');
+const rxjs = require('rxjs');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAvgAdnejzqDmSwz2USui_-Gfu4jSHh-v4",
+    authDomain: "swimapp-d4f4a.firebaseapp.com",
+    projectId: "swimapp-d4f4a"
+});
+
+const db = firebase.firestore();
+
+
+
+module.exports = {
+    authenticate,
+    getAll
+};
+
+function authenticate({ username, password }) {
+    const users = db.collection('users').where('username', '==', username);
+    return users.get().then(async (querySnapshot) => {
+        const userDoc = querySnapshot.docs.find(doc => {
+            return doc.data().username.toUpperCase() === username.toUpperCase();
+        })
+
+        if (userDoc) {
+            let user = userDoc.data();
+            const pwdCompareResult = await bcrypt.compare(password, user.password);
+
+            console.log(password);
+            console.log(user.password);
+            console.log(pwdCompareResult);
+
+            if (pwdCompareResult) {
+                const token = jwt.sign({ sub: user.id }, config.secret);
+                const { password, ...userWithoutPassword } = user;
+                return {
+                    ...userWithoutPassword,
+                    token
+                }
+            }
+        }
+    })
+}
+
+async function getAll() {
+    return users.map(u => {
+        const { password, ...userWithoutPassword } = u;
+        return userWithoutPassword;
+    });
+}
