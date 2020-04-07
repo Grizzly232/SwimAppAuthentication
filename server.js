dcb35f824c85266d34063e97807d1dda366f2067
Michael Brady <michael.brady.ie@gmail.com>
2020-04-07 14:58:19 +0100

    fixed issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const requestListener = function(req, res) {
 }
 
 const port = process.env.PORT || 3000;
-const server = httpServer.createServer();
+const server = httpServer.createServer(requestListener);
 server.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
